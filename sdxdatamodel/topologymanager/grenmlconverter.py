5c829fb99f61236f4b6e38e75b6e964e24dd5cac
Sajith Sasidharan <sajith@hcoop.net>
2022-09-13 21:59:36 -0230

    Remove unused imports

--- sdxdatamodel/topologymanager/grenmlconverter.py
+++ sdxdatamodel/topologymanager/grenmlconverter.py
@@ -1,11 +1,7 @@
 from grenml import GRENMLManager
 from grenml.models.nodes import Node
-from grenml.models.links import Link
 
 from sdxdatamodel.models.topology import Topology
-
-# from models.node import Node
-from sdxdatamodel.models.location import Location
 
 
 class GrenmlConverter(object):
