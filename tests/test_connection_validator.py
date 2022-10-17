b5c0fec277a861ec40416caa103f2d4c5c6d0978
Sajith Sasidharan <sajith@hcoop.net>
2022-10-17 14:51:01 -0500

    Validate another JSON doc describing a connection

--- tests/test_connection_validator.py
+++ tests/test_connection_validator.py
@@ -10,28 +10,40 @@ from sdxdatamodel.models.connection import Connection
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 CONNECTION_P2P = os.path.join(TEST_DATA_DIR, "p2p.json")
-# CONNECTION_P2P = './tests/data/test_connection.json'
+CONNECTION_REQ = os.path.join(TEST_DATA_DIR, "test_request.json")
 
 
 class TestConnectionValidator(unittest.TestCase):
-
     def setUp(self):
         pass
-    
+
     def tearDown(self):
         pass
 
-    def test_connection_json(self):
+    def _get_validator(self, filename):
         """
-        Test that a JSON document that describes a connection can be
-        validated.
+        Return a validator for the given file.
         """
         handler = ConnectionHandler()
-        conn = handler.import_connection(CONNECTION_P2P)
+        conn = handler.import_connection(filename)
 
         validator = ConnectionValidator()
         validator.set_connection(conn)
 
+        return validator
+
+    def test_connection_json_p2p(self):
+        """
+        Validate a JSON document descibing a connection.
+        """
+        validator = self._get_validator(CONNECTION_P2P)
+        self.assertTrue(validator.is_valid())
+
+    def test_connection_json_req(self):
+        """
+        Validate a JSON document descibing a connection.
+        """
+        validator = self._get_validator(CONNECTION_REQ)
         self.assertTrue(validator.is_valid())
 
 
