e7cb117d34fade77eb8d1cec5371e7120dbd5361
Sajith Sasidharan <sajith@hcoop.net>
2022-10-17 15:23:27 -0500

    Add a test for connection object validation

--- tests/test_connection_validator.py
+++ tests/test_connection_validator.py
@@ -1,9 +1,12 @@
+import datetime
+import json
 import os
 import unittest
 
+from sdxdatamodel.models.connection import Connection
+from sdxdatamodel.models.port import Port
 from sdxdatamodel.parsing.connectionhandler import ConnectionHandler
 from sdxdatamodel.validation.connectionvalidator import ConnectionValidator
-
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 CONNECTION_P2P = os.path.join(TEST_DATA_DIR, "p2p.json")
@@ -42,6 +45,39 @@ class TestConnectionValidator(unittest.TestCase):
         """
         validator = self._get_validator(CONNECTION_REQ)
         self.assertTrue(validator.is_valid())
+
+    def test_connection_object(self):
+        """
+        Create a connection object and validate it.
+        """
+        ingress_port = Port(
+            id="ingress_port_id",
+            name="ingress_port_name",
+            node="ingress_node_name",
+            status="unknown",
+        )
+
+        egress_port = Port(
+            id="egress_port_id",
+            name="egress_port_name",
+            node="egress_node_name",
+            status="unknown",
+        )
+        
+        connection = Connection(
+            id="test_place_connection_id",
+            name="test_place_connection_name",
+            ingress_port=json.dumps(ingress_port),
+            egress_port=json.dumps(egress_port),
+            quantity=0,
+            start_time=datetime.datetime.fromtimestamp(0),
+            end_time=datetime.datetime.fromtimestamp(0),
+            status="fail",
+            complete=False,
+        )
+
+        data = json.dumps(connection)
+        print(data)
 
 
 if __name__ == "__main__":
