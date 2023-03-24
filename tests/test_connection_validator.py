619a09b36fde6f4ad93a629e31d2a818a2e40ecc
Sajith Sasidharan <sajith@hcoop.net>
2023-03-24 16:07:57 -0500

    Make variables more local

--- tests/test_connection_validator.py
+++ tests/test_connection_validator.py
@@ -6,18 +6,16 @@ from sdx.datamodel.parsing.connectionhandler import ConnectionHandler
 
 
 class TestConnectionValidator(unittest.TestCase):
-    def test_connection_validator(self):
-        CONNECTION_P2P = pathlib.Path(__file__).parent.joinpath(
-            "data", "p2p.json"
-        )
+    TEST_DATA_DIR = pathlib.Path(__file__).parent.joinpath("data")
+    CONNECTION_P2P = TEST_DATA_DIR.joinpath("p2p.json")
 
-        self.handler = ConnectionHandler()
-        connection = self.handler.import_connection(CONNECTION_P2P)
+    def test_connection_validator(self):
+        connection = ConnectionHandler().import_connection(self.CONNECTION_P2P)
         print(f"Imported Connection: {connection}")
 
-        self.validator = ConnectionValidator()
-        self.validator.set_connection(connection)
-        self.assertTrue(self.validator.is_valid())
+        validator = ConnectionValidator()
+        validator.set_connection(connection)
+        self.assertTrue(validator.is_valid())
 
 
 if __name__ == "__main__":
