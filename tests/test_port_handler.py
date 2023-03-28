f024f00d4748279bd0d86001b49de21f757e5140
Sajith Sasidharan <sajith@hcoop.net>
2023-03-28 15:41:50 -0500

    Update port handler test

--- tests/test_port_handler.py
+++ tests/test_port_handler.py
@@ -1,28 +1,22 @@
-import os
+import pathlib
 import unittest
 
 from sdx.datamodel.models.port import Port
-from sdx.datamodel.parsing.exceptions import DataModelException
 from sdx.datamodel.parsing.porthandler import PortHandler
 
-TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
-PORT_DATA = os.path.join(TEST_DATA_DIR, "port.json")
 
-
-class TestPortHandler(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
+class PortHandlerTests(unittest.TestCase):
+    TEST_DATA_DIR = pathlib.Path(__file__).parent.joinpath("data")
+    PORT_DATA_FILE = TEST_DATA_DIR.joinpath("port.json")
 
     def test_import_port_json(self):
         """
         Test that a Port object can be created given a JSON descritpion of a port.
         """
         handler = PortHandler()
+
         # import_port() must not raise a DataModelException.
-        port = handler.import_port(PORT_DATA)
+        port = handler.import_port(self.PORT_DATA_FILE)
         self.assertIsInstance(port, Port)
 
 
