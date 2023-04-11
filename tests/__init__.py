f061010068adb5e615b3115139b222fd7993b59a
Sajith Sasidharan <sajith@hcoop.net>
2023-04-11 11:05:50 -0500

    Add tests for L2VPN port data

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,6 +13,16 @@ class TestData:
     PORT_FILE = TEST_DATA_DIR.joinpath("port.json")
     SERVICE_FILE = TEST_DATA_DIR.joinpath("service.json")
 
+    PORT_FILE_L2VPN_PTP = TEST_DATA_DIR.joinpath("port-l2vpn-ptp.json")
+    PORT_FILE_L2VPN_PTP_BAD = TEST_DATA_DIR.joinpath("port-l2vpn-ptp-bad.json")
+
+    PORT_FILE_L2VPN_PTP_PTMP = TEST_DATA_DIR.joinpath(
+        "port-l2vpn-ptp-ptmp.json"
+    )
+    PORT_FILE_L2VPN_PTP_PTMP_BAD = TEST_DATA_DIR.joinpath(
+        "port-l2vpn-ptp-ptmp-bad.json"
+    )
+
     TOPOLOGY_AMLIGHT = TEST_DATA_DIR.joinpath("amlight.json")
     TOPOLOGY_AMPATH = TEST_DATA_DIR.joinpath("ampath.json")
     TOPOLOGY_SAX = TEST_DATA_DIR.joinpath("sax.json")
--- tests/test_port_handler.py
+++ tests/test_port_handler.py
@@ -7,6 +7,10 @@ from . import TestData
 
 
 class PortHandlerTests(unittest.TestCase):
+    """
+    Tests for port data parsing logic.
+    """
+
     def test_import_port_json(self):
         """
         Test that a Port object can be created given a JSON descritpion of a port.
@@ -16,6 +20,46 @@ class PortHandlerTests(unittest.TestCase):
         # import_port() must not raise a DataModelException.
         port = handler.import_port(TestData.PORT_FILE)
         self.assertIsInstance(port, Port)
+
+    def test_import_port_json_l2vpn_ptp(self):
+        """
+        Test that a Port object can be created given a JSON descritpion of a port.
+        """
+        handler = PortHandler()
+
+        # import_port() must not raise a DataModelException.
+        port = handler.import_port(TestData.PORT_FILE_L2VPN_PTP)
+        self.assertIsInstance(port, Port)
+
+    def test_import_port_json_l2vpn_ptp_bad(self):
+        """
+        Test that a Port object can be created given a JSON descritpion of a port.
+        """
+        handler = PortHandler()
+
+        # import_port() must not raise a DataModelException.
+        port = handler.import_port(TestData.PORT_FILE_L2VPN_PTP_BAD)
+        self.assertIsNone(port)
+
+    def test_import_port_json_l2vpn_ptp_and_ptmp(self):
+        """
+        Test that a Port object can be created given a JSON descritpion of a port.
+        """
+        handler = PortHandler()
+
+        # import_port() must not raise a DataModelException.
+        port = handler.import_port(TestData.PORT_FILE_L2VPN_PTP_PTMP)
+        self.assertIsInstance(port, Port)
+
+    def test_import_port_json_l2vpn_ptp_and_ptmp(self):
+        """
+        Test that a Port object can be created given a JSON descritpion of a port.
+        """
+        handler = PortHandler()
+
+        # import_port() must not raise a DataModelException.
+        port = handler.import_port(TestData.PORT_FILE_L2VPN_PTP_PTMP_BAD)
+        self.assertIsNone(port)
 
 
 if __name__ == "__main__":
