6c9777aba3ec69c891f600a703553b7388d00afb
Sajith Sasidharan <sajith@hcoop.net>
2023-08-04 10:20:37 -0500

    Rename test method to avoid duplicate

--- tests/test_port_handler.py
+++ tests/test_port_handler.py
@@ -52,7 +52,7 @@ class PortHandlerTests(unittest.TestCase):
         port = handler.import_port(TestData.PORT_FILE_L2VPN_PTP_PTMP)
         self.assertIsInstance(port, Port)
 
-    def test_import_port_json_l2vpn_ptp_and_ptmp(self):
+    def test_import_port_json_l2vpn_ptp_and_ptmp_bad(self):
         """
         Test that a Port object can be created given a JSON descritpion of a port.
         """
