f60e655ac84eed73ba1cfd60b8b0c31c28da4ee2
Sajith Sasidharan <sajith@hcoop.net>
2023-08-17 20:37:15 -0500

    Use jsonref to fully expand $ref in schemas

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -2,6 +2,7 @@ import json
 import unittest
 from pathlib import Path
 
+import jsonref
 import jsonschema
 
 from . import TestData
@@ -29,53 +30,61 @@ class JSONSchemaTests(unittest.TestCase):
         """
         return json.loads(path.read_text())
 
+    def _read_schema(self, path: Path):
+        """
+        Read schema and return a dict with expanded $ref.
+        """
+        return jsonref.loads(
+            path.read_text(), base_uri=path.absolute().as_uri()
+        )
+
     def test_connection_request_schema(self):
         jsonschema.validate(
             self._read_json(TestData.CONNECTION_FILE_REQ),
-            self._read_json(self.CONNECTION_SCHEMA_FILE),
+            self._read_schema(self.CONNECTION_SCHEMA_FILE),
         )
 
     def test_link_schema(self):
         jsonschema.validate(
             self._read_json(TestData.LINK_FILE),
-            self._read_json(self.LINK_SCHEMA_FILE),
+            self._read_schema(self.LINK_SCHEMA_FILE),
         )
 
     def test_location_schema(self):
         jsonschema.validate(
             self._read_json(TestData.LOCATION_FILE),
-            self._read_json(self.LOCATION_SCHEMA_FILE),
+            self._read_schema(self.LOCATION_SCHEMA_FILE),
         )
 
     def test_node_schema(self):
         jsonschema.validate(
             self._read_json(TestData.NODE_FILE),
-            self._read_json(self.NODE_SCHEMA_FILE),
+            self._read_schema(self.NODE_SCHEMA_FILE),
         )
 
     @unittest.skip(reason="Path files are not implemented.")
     def test_path_schema(self):
         jsonschema.validate(
             self._read_json(TestData.PATH_FILE),
-            self._read_json(self.PATH_SCHEMA_FILE),
+            self._read_schema(self.PATH_SCHEMA_FILE),
         )
 
     def test_port_schema(self):
         jsonschema.validate(
             self._read_json(TestData.PORT_FILE),
-            self._read_json(self.PORT_SCHEMA_FILE),
+            self._read_schema(self.PORT_SCHEMA_FILE),
         )
 
     def test_service_schema(self):
         jsonschema.validate(
             self._read_json(TestData.SERVICE_FILE),
-            self._read_json(self.SERVICE_SCHEMA_FILE),
+            self._read_schema(self.SERVICE_SCHEMA_FILE),
         )
 
     def test_topology_schema(self):
         jsonschema.validate(
             self._read_json(TestData.TOPOLOGY_FILE_AMLIGHT),
-            self._read_json(self.TOPOLOGY_SCHEMA_FILE),
+            self._read_schema(self.TOPOLOGY_SCHEMA_FILE),
         )
 
 
