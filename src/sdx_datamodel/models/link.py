69fc2c9b327393f3b4fa55075e3650eed14a1540
Yufeng Xin <yxin@renci.org>
2024-05-06 16:00:39 -0400

    minor

--- src/sdx_datamodel/models/link.py
+++ src/sdx_datamodel/models/link.py
@@ -6,15 +6,12 @@ from datetime import date, datetime  # noqa: F401
 from typing import Dict, List  # noqa: F401
 
 from sdx_datamodel import util
-from sdx_datamodel.handlers.link_measurement_period_handler import (
-    link_measurement_period_handler,
-)
 from sdx_datamodel.handlers.port_handler import port_handler
 from sdx_datamodel.models.base_model_ import Model
 from sdx_datamodel.models.link_measurement_period import (  # noqa: F401,E501
     LinkMeasurementPeriod,
 )
-from sdx_datamodel.models.port import Port  # noqa: F401,E501
+from sdx_datamodel.models.port import Port  # noqa: F401,E501s
 
 
 class Link(Model):
