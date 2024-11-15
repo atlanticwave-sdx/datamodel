d9eac87c18df3bd95cf19e3512c5492c1ed33bc0
Yufeng Xin <yxin@renci.org>
2024-11-15 13:02:18 -0500

    black

--- src/sdx_datamodel/connection_sm.py
+++ src/sdx_datamodel/connection_sm.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from transitions import Machine
 from transitions.extensions import GraphMachine
 
+
 class ConnectionStateMachine:
     states = [
         "requested",
@@ -21,7 +22,7 @@ class ConnectionStateMachine:
         "provisioning": ["success", "failed"],
         "success": ["modified", "failed", "deleted"],
         "modified": ["success", "failed"],
-        "failed": ["recovering","deleted"],
+        "failed": ["recovering", "deleted"],
         "recovering": ["success"],
     }
 
@@ -34,8 +35,12 @@ class ConnectionStateMachine:
             logger.info(f"Transitioning from {self.state} to {new_state}")
             self.state = new_state
         else:
-            logger.error(f"Invalid transition from {self.state} to {new_state}")
-            raise ValueError(f"Cannot transition from {self.state} to {new_state}")
+            logger.error(
+                f"Invalid transition from {self.state} to {new_state}"
+            )
+            raise ValueError(
+                f"Cannot transition from {self.state} to {new_state}"
+            )
 
     def get_state(self):
         return self.state
@@ -48,15 +53,28 @@ class ConnectionStateMachine:
 
     def set_state(self, state):
         self.state = state
-    
+
     def draw_state_machine(self):
         G = nx.DiGraph()
         G.add_nodes_from(ConnectionStateMachine.states)
         for key, value in ConnectionStateMachine.transitions.items():
             for v in value:
-                G.add_edge(key, v) 
+                G.add_edge(key, v)
         pos = nx.planar_layout(G)
-        nx.draw(G, pos, with_labels=True, node_size=2000, node_color="orange", font_size=10, font_color="black", font_weight="bold", edge_color="blue", width=2, style="dashed", alpha=0.9)
+        nx.draw(
+            G,
+            pos,
+            with_labels=True,
+            node_size=2000,
+            node_color="orange",
+            font_size=10,
+            font_color="black",
+            font_weight="bold",
+            edge_color="blue",
+            width=2,
+            style="dashed",
+            alpha=0.9,
+        )
         plt.savefig(self.SM_PNG)
         plt.show()
         return G
@@ -65,17 +83,26 @@ class ConnectionStateMachine:
         transitions = []
         for key, value in ConnectionStateMachine.transitions.items():
             for v in value:
-                transition = {'trigger': '', 'source': key, 'dest': v}
+                transition = {"trigger": "", "source": key, "dest": v}
                 transitions.append(transition)
 
-        machine = GraphMachine(model=self, graph_engine="graphviz", states=ConnectionStateMachine.states, transitions=transitions, initial="requested", title="Connection State Machine", show_conditions=True)
-        
-        #machine.get_graph().draw("connection_state_machine.png", prog="dot")
-        with open('transition.png', 'bw') as f:
+        machine = GraphMachine(
+            model=self,
+            graph_engine="graphviz",
+            states=ConnectionStateMachine.states,
+            transitions=transitions,
+            initial="requested",
+            title="Connection State Machine",
+            show_conditions=True,
+        )
+
+        # machine.get_graph().draw("connection_state_machine.png", prog="dot")
+        with open("transition.png", "bw") as f:
             # you need to pass the format when you pass objects instead of filenames.
-            machine.get_graph().draw(f, format='png', prog='dot')
+            machine.get_graph().draw(f, format="png", prog="dot")
+
 
 if __name__ == "__main__":
-    sm=ConnectionStateMachine()
+    sm = ConnectionStateMachine()
     sm.draw_transition()
-    g=sm.draw_state_machine()
+    g = sm.draw_state_machine()
