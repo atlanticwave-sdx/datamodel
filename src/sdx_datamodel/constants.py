89549d3958ef9c308758c359c6293f3241a5821f
Cong Wang <cwang@renci.org>
2025-02-17 15:45:54 -0500

    Add constants

---
+++ src/sdx_datamodel/constants.py
@@ -0,0 +1,16 @@
+class MongoCollections:
+    TOPOLOGIES = "topologies"
+    CONNECTIONS = "connections"
+    BREAKDOWNS = "breakdowns"
+    DOMAINS = "domains"
+    LINKS = "links"
+    HISTORICAL_CONNECTIONS = "historical_connections"
+
+
+class Constants:
+    DOMAIN_LIST = "domain_list"
+    LATEST_TOPO = "latest_topo"
+    LINK_CONNECTIONS_DICT = "link_connections_dict"
+    LATEST_TOPOLGY = "latest_topology"
+    LATEST_TOPOLOGY_TS = "latest_topology_ts"
+    TOPOLOGY_VERSION = "topology_version"
