530546d41a0e776a1a75e41a92ba966837f23064
Cong Wang <cwang@renci.org>
2025-02-17 16:01:37 -0500

    Add MQ consts

--- src/sdx_datamodel/constants.py
+++ src/sdx_datamodel/constants.py
@@ -14,3 +14,9 @@ class Constants:
     LATEST_TOPOLGY = "latest_topology"
     LATEST_TOPOLOGY_TS = "latest_topology_ts"
     TOPOLOGY_VERSION = "topology_version"
+
+
+class MessageQueueNames:
+    OXP_UPDATE = "oxp_update"
+    CONNECTIONS = "connections"
+    SDX_QUEUE_1 = "sdx_queue_1"
