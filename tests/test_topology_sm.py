deef5987ed72af1dd68157c3970604d4c5391a0b
Yufeng Xin <yxin@renci.org>
2025-02-18 12:33:59 -0500

    modified topology sm

--- tests/test_topology_sm.py
+++ tests/test_topology_sm.py
@@ -10,53 +10,83 @@ class TestTopologyStateMachine(unittest.TestCase):
     def test_initial_state(self):
         self.assertEqual(self.sm.state, self.sm.State.START)
 
-    def test_transition_start_to_db_update(self):
-        self.sm.transition(self.sm.State.DB_UPDATE)
-        self.assertEqual(self.sm.state, self.sm.State.DB_UPDATE)
+    def test_transition_start_to_oxp_update(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.assertEqual(self.sm.state, self.sm.State.OXP_UPDATE)
 
-    def test_transition_start_to_orphaned(self):
-        self.sm.transition(self.sm.State.ORPHANED)
-        self.assertEqual(self.sm.state, self.sm.State.ORPHANED)
+    def test_transition_start_to_error(self):
+        self.sm.transition(self.sm.State.ERROR)
+        self.assertEqual(self.sm.state, self.sm.State.ERROR)
 
-    def test_invalid_transition(self):
+    def test_invalid_transition_start_to_up(self):
         with self.assertRaises(ValueError):
-            self.sm.transition(self.sm.State.DELETED)
+            self.sm.transition(self.sm.State.UP)
 
-    def test_transition_db_update_to_db_update(self):
-        self.sm.transition(self.sm.State.DB_UPDATE)
-        self.sm.transition(self.sm.State.DB_UPDATE)
-        self.assertEqual(self.sm.state, self.sm.State.DB_UPDATE)
+    def test_transition_oxp_update_to_up(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.sm.transition(self.sm.State.UP)
+        self.assertEqual(self.sm.state, self.sm.State.UP)
 
-    def test_transition_db_update_to_orphaned(self):
-        self.sm.transition(self.sm.State.DB_UPDATE)
-        self.sm.transition(self.sm.State.ORPHANED)
-        self.assertEqual(self.sm.state, self.sm.State.ORPHANED)
+    def test_transition_up_to_oxp_update(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.sm.transition(self.sm.State.UP)
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.assertEqual(self.sm.state, self.sm.State.OXP_UPDATE)
 
-    def test_transition_orphaned_to_db_update(self):
-        self.sm.transition(self.sm.State.ORPHANED)
-        self.sm.transition(self.sm.State.DB_UPDATE)
-        self.assertEqual(self.sm.state, self.sm.State.DB_UPDATE)
+    def test_transition_up_to_prov_update(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.sm.transition(self.sm.State.UP)
+        self.sm.transition(self.sm.State.PROV_UPDATE)
+        self.assertEqual(self.sm.state, self.sm.State.PROV_UPDATE)
 
-    def test_transition_db_update_to_deleted(self):
-        self.sm.transition(self.sm.State.DB_UPDATE)
-        self.sm.transition(self.sm.State.DELETED)
-        self.assertEqual(self.sm.state, self.sm.State.DELETED)
+    def test_transition_prov_update_to_up(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.sm.transition(self.sm.State.UP)
+        self.sm.transition(self.sm.State.PROV_UPDATE)
+        self.sm.transition(self.sm.State.UP)
+        self.assertEqual(self.sm.state, self.sm.State.UP)
 
-    def test_transition_orphaned_to_deleted(self):
-        self.sm.transition(self.sm.State.ORPHANED)
-        self.sm.transition(self.sm.State.DELETED)
-        self.assertEqual(self.sm.state, self.sm.State.DELETED)
+    def test_transition_up_to_error(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.sm.transition(self.sm.State.UP)
+        self.sm.transition(self.sm.State.ERROR)
+        self.assertEqual(self.sm.state, self.sm.State.ERROR)
 
-    def test_transition_db_update_to_maintenance(self):
-        self.sm.transition(self.sm.State.DB_UPDATE)
+    def test_transition_error_to_oxp_update(self):
+        self.sm.transition(self.sm.State.ERROR)
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.assertEqual(self.sm.state, self.sm.State.OXP_UPDATE)
+
+    def test_transition_up_to_maintenance(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.sm.transition(self.sm.State.UP)
         self.sm.transition(self.sm.State.MAINTENANCE)
         self.assertEqual(self.sm.state, self.sm.State.MAINTENANCE)
 
-    def test_transition_maintenance_to_db_update(self):
-        self.sm.set_state(self.sm.State.DB_UPDATE)
+    def test_transition_maintenance_to_up(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.sm.transition(self.sm.State.UP)
         self.sm.transition(self.sm.State.MAINTENANCE)
-        self.sm.transition(self.sm.State.DB_UPDATE)
-        self.assertEqual(self.sm.state, self.sm.State.DB_UPDATE)
+        self.sm.transition(self.sm.State.UP)
+        self.assertEqual(self.sm.state, self.sm.State.UP)
+
+    def test_transition_up_to_deleted(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.sm.transition(self.sm.State.UP)
+        self.sm.transition(self.sm.State.DELETED)
+        self.assertEqual(self.sm.state, self.sm.State.DELETED)
+
+    def test_transition_error_to_deleted(self):
+        self.sm.transition(self.sm.State.ERROR)
+        self.sm.transition(self.sm.State.DELETED)
+        self.assertEqual(self.sm.state, self.sm.State.DELETED)
+
+    def test_transition_maintenance_to_deleted(self):
+        self.sm.transition(self.sm.State.OXP_UPDATE)
+        self.sm.transition(self.sm.State.UP)
+        self.sm.transition(self.sm.State.MAINTENANCE)
+        self.sm.transition(self.sm.State.DELETED)
+        self.assertEqual(self.sm.state, self.sm.State.DELETED)
 
 
 if __name__ == "__main__":
