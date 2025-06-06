a68abf60486218a29d150e44cab73163ceb3d6ff
Cong Wang <cwang@renci.org>
2025-06-06 15:18:05 -0400

    Add port_connections_dict

--- src/sdx_datamodel/constants.py
+++ src/sdx_datamodel/constants.py
@@ -11,6 +11,7 @@ class MongoCollections:
 class Constants:
     DOMAIN_LIST = "domain_list"
     LINK_CONNECTIONS_DICT = "link_connections_dict"
+    PORT_CONNECTIONS_DICT = "port_connections_dict"
     LATEST_TOPOLOGY = "latest_topology"
     LATEST_TOPOLOGY_TS = "latest_topology_ts"
     TOPOLOGY_VERSION = "topology_version"
